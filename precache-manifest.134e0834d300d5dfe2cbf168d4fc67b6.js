86ace949161b0e9dd9953378f60fb808d3d0428e
Shubham Shingare <shubhamshingare@Shubham.local>
2020-03-08 17:47:27 +0530

    Updates

---
+++ precache-manifest.134e0834d300d5dfe2cbf168d4fc67b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0080e147c45e6b17ee10165cbc3f3a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a32bfd960eba39c4db3e",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08cd1ac7ef520700efd5",
+    "url": "/Portfolio/static/js/2.eabf0129.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Portfolio/static/js/2.eabf0129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32bfd960eba39c4db3e",
+    "url": "/Portfolio/static/js/main.e81dc1b2.chunk.js"
+  },
+  {
+    "revision": "654ea12aa83236a26bec",
+    "url": "/Portfolio/static/js/runtime-main.e1c49509.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.359abbe597777d0e0164a981b2fa9c23.js"
+  "/Portfolio/precache-manifest.134e0834d300d5dfe2cbf168d4fc67b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
